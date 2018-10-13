a25fb523f314d3c249c24e114d4618db3ac8d974
Sergey Kuzmin <Sergey_Kuzmin1@epam.com>
2018-10-13 14:47:54 +0400

    HOL2. Add runtime-specific code

--- src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -21,14 +22,33 @@ namespace EPAM.Core.ReportHelper.Tests
 		}
 
 		[TestMethod]
-		//[DataRow("Hello: my world", '_', "Hello_ my world")]
-		[DataRow("Hello my %world", '_', "Hello my %world")]
-		[DataRow("Hello my /world", '_', "Hello my _world")]
+		[DynamicData("NormalizeFileNameTestData", DynamicDataSourceType.Property)]
 		public void NormalizeFileNameTest(string name, char repl, string expected)
 		{
 			var result = ReportNameHelper.NormalizeFileName(name, repl);
 
 			result.Should().BeEquivalentTo(expected);
+		}
+
+		static object[][] NormalizeFileNameTestData
+		{
+			get
+			{
+				object[][] data = new object[][]
+				{
+					new object[] {"Hello: my world", '_', "Hello_ my world"},
+					new object[] {"Hello my %world", '_', "Hello my %world"},
+					new object[] {"Hello my /world", '_', "Hello my _world"}
+				};
+
+				if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+				{
+					data[0][2] = "Hello: my world";
+					data[1][2] = "Hello my %world";
+					data[2][2] = "Hello my _world";
+				}
+				return data;
+			}
 		}
 	}
 }
